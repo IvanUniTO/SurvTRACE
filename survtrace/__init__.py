c1a4dc36175b8e1b58fb18cf6645bde68fef5697
Ivan Rossi <ivan@biodec.com>
2024-03-22 17:17:35 +0100

    chg: relaxed deps wrt. pytorch

--- survtrace/__init__.py
+++ survtrace/__init__.py
@@ -1,6 +1,6 @@
 "This is a fork of the original SurvTRACE code, to be used in COMPBIOMED-UNITO projects"
 
-__version__ = "1.0.2+hive"
+__version__ = "1.0.3+hive"
 
 from .evaluate_utils import Evaluator
 from .train_utils import Trainer
