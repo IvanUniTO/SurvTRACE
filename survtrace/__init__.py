7fb8072f846c1dd92d31296feb5dde37205cbd5c
Ivan Rossi <ivan@biodec.com>
2024-06-05 11:47:19 +0200

    Modified dependency syntax to latest packaging specs

--- survtrace/__init__.py
+++ survtrace/__init__.py
@@ -1,6 +1,6 @@
 "This is a fork of the original SurvTRACE code, to be used in COMPBIOMED-UNITO projects"
 
-__version__ = "1.0.3+hive"
+__version__ = "1.0.4+hive"
 
 from .evaluate_utils import Evaluator
 from .train_utils import Trainer
